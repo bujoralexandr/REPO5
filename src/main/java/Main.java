82ab55de660c148bbc606660ccc8ea6ae6fcc96c
bujoralexandr <bujor.alexandru.98@gmail.com>
2020-10-28 14:05:52 +0200

    Merging commits
    Add Two Values Method
    
    Add Three Values Method
    
    Add another method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,17 @@ public class Main {
 
         System.out.println("Hello!");
     }
+    public int addTwoValues(int x, int y) {
+
+        return x + y;
+    }
+    public int addThreeValues(int x, int y, int z) {
+
+        return x + y + z;
+    }
+    public static void showInfo() {
+
+        System.out.println("This is Basic class");
+        System.out.format("The Id is: %d%n", id);
+    }
 }
