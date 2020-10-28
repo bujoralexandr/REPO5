9a59fe36261bf27ea55fdc7719115f367b79a4d5
bujoralexandr <bujor.alexandru.98@gmail.com>
2020-10-28 04:39:00 +0200

    Adding several methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,4 +6,10 @@ public class Main {
     public static int getIntegerSum (int i, int j) {
         return i + j;
     }
+    public static int multiplyInteger (int x, int y) {
+        return x * y;
+    }
+    private static int getSquare(int x){
+        return x * x;
+    }
 }
