2a5ac128251a67c71f147230155bf29b6b60a5d0
bujoralexandr <bujor.alexandru.98@gmail.com>
2020-10-28 04:08:42 +0200

    Change main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main( String[] args) {
 
-        System.out.println("Hello!");
+        System.out.println("Changing this instead");
     }
 
-
-
-    public static int multiplyInteger (int x, int y) {
-        return x * y;
-    }
 }
